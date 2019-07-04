2cf45e653416deb1a8b30f1dd49ffc1405fbb127
duanpp <duanpp@rongzhijia.com>
2019-07-04 22:37:33 +0800

    go learn 3

---
+++ src/ch5/condition/condition_test.go
@@ -0,0 +1,40 @@
+package condition
+
+import "testing"
+
+func TestIfMultiSec(t *testing.T) {
+	if a := 1 == 1; a {
+		t.Log("1==1")
+	}
+	/*	if v,err := someFunc();err == nil {
+			t.Log("ok")
+		}else{
+			t.Log("error")
+		}*/
+}
+
+func TestSwitchMultiCase(t *testing.T) {
+	for i := 0; i < 5; i++ {
+		switch i {
+		case 0, 2:
+			t.Log("Even")
+		case 1, 3:
+			t.Log("Odd")
+		default:
+			t.Log("it is not  0-3")
+		}
+	}
+}
+
+func TestSwitchCaseCondition(t *testing.T) {
+	for i := 0; i < 5; i++ {
+		switch {
+		case i%2 == 0:
+			t.Log("Even")
+		case i%2 == 1:
+			t.Log("Odd")
+		default:
+			t.Log("unknow")
+		}
+	}
+}
